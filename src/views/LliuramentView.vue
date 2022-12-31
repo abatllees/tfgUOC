9e68165ad69397d8f3e9b06335d239ad3b399265
Albert Batlle Espinalt <abatllees@uoc.edu>
2022-12-31 15:58:28 +0100

    Now you can remove elements from LliuramentList

--- src/views/LliuramentView.vue
+++ src/views/LliuramentView.vue
@@ -16,7 +16,14 @@
             :sort-by="this.sortBy" :sort-type="this.sortType" :theme-color="this.$store.state.themeColor">
             <template #item-operation="item">
                 <div class="operation-wrapper">
-                    <img src="../assets/delete.png" alt="delete" class="operation-icon" @click="deleteItem(item)" />
+                    <i class="bi bi-trash"></i>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-trash" viewBox="0 0 16 16" @click="deleteItem(item)">
+                        <path
+                            d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                        <path fill-rule="evenodd"
+                            d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
+                    </svg>
                 </div>
             </template>
         </EasyDataTable>
@@ -68,6 +75,11 @@ export default {
                 }
             }
             this.$store.dispatch("exportToPDF", payload)
+        },
+        deleteItem(item) {
+            const findItem = this.$store.state.llistatMoviment.find(e => e.SerialNum == item.SerialNum);
+            const index = this.$store.state.llistatMoviment.indexOf(findItem);
+            this.$store.state.llistatMoviment.splice(index, 1)
         }
     }
 }
